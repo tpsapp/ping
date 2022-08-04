04e9619cf87e734d06d6642f7d8c86818bee1e6a
Thomas Sapp <53871494+tpsapp@users.noreply.github.com>
2022-08-04 15:04:04 -0700

    Create settings.py

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+screen_width = 800
+screen_height = 600
+screen_caption = "Ping - A Pong Clone"
+framerate = 60
+screen_update_time = 30
+font_size = 32
+font_face = 'Lucida Console'
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+winning_score = 5
