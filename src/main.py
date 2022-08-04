4fbaf6223f583c381edc7a4b42c67c47a7d6b454
Thomas Sapp <53871494+tpsapp@users.noreply.github.com>
2022-08-04 15:03:24 -0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+import settings
+from game import GAME
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+pygame.display.set_caption(settings.screen_caption)
+game = GAME(screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            game.handle_keydown(event.key)
+        if event.type == pygame.KEYUP:
+            game.handle_keyup(event.key)
+
+    game.update()
+    screen.fill(settings.black)
+    game.draw()
+    pygame.display.flip()
+    clock.tick(settings.framerate)
